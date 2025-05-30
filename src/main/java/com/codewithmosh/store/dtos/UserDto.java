882c7e7b881dda904c836a974276f66bedaa11f1
cenesdeveloper <cenesdurak@gmail.com>
2025-05-30 06:42:19 -0400

    Update pom.xml

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class UserDto {
-    private final Long id;
-    private final String name;
-    private final String email;
+    private Long id;
+    private String name;
+    private String email;
 }
