d67e6083c2986dbb0b5c3ddcbbc1734a334d68c6
cenesdeveloper <cenesdurak@gmail.com>
2025-05-29 23:52:23 -0400

    Added user dto

---
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private final Long id;
+    private final String name;
+    private final String email;
+}
